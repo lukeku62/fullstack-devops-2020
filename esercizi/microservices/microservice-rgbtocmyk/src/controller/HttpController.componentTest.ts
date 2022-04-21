111c1571d4e10e1147325b4414641b9658d93112
Luca Reverberi (lukeku62) <prevebbo@gmail.com>
2022-04-21 12:56:58 +0200

    implementato servizio ms-rgbtocmyk

--- esercizi/microservices/microservice-rgbtocmyk/src/controller/HttpController.componentTest.ts
+++ esercizi/microservices/microservice-rgbtocmyk/src/controller/HttpController.componentTest.ts
@@ -1,26 +1,61 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import * as config from '../../server-config.json'
+import chai from "chai";
+import chaiHttp from "chai-http";
+import * as config from "../../server-config.json";
+
+type TestType = {
+  input: string;
+  expected: { inputRGB: number[]; outputCMYK: number[] };
+};
+
+const testData: TestType[] = [
+  {
+    input: '{"r":0, "g":0, "b":0}',
+    expected: { inputRGB: [0, 0, 0], outputCMYK: [0, 0, 0, 100] },
+  },
+  {
+    input: '{"r":255, "g":0, "b":0}',
+    expected: { inputRGB: [255, 0, 0], outputCMYK: [0, 100, 100, 0] },
+  },
+  {
+    input: '{"r":0, "g":255, "b":0}',
+    expected: { inputRGB: [0, 255, 0], outputCMYK: [100, 0, 100, 0] },
+  },
+  {
+    input: '{"r":0, "g":0, "b":255}',
+    expected: { inputRGB: [0, 0, 255], outputCMYK: [100, 100, 0, 0] },
+  },
+  {
+    input: '{"r":255, "g":255, "b":0}',
+    expected: { inputRGB: [255, 255, 0], outputCMYK: [0, 0, 100, 0] },
+  },
+  {
+    input: '{"r":0, "g":255, "b":255}',
+    expected: { inputRGB: [0, 255, 255], outputCMYK: [100, 0, 0, 0] },
+  },
+];
 
 chai.config.includeStack = true;
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('REST API test suite description', () => {
-    const url = process.env.npm_config_rgb2hex_test_url || `http://localhost:${config.port}`;
-    console.log('Test URL: ' + url);
+describe("REST API test suite description", () => {
+  const url =
+    process.env.npm_config_rgb2hex_test_url ||
+    `http://localhost:${config.port}`;
+  console.log("Test URL: " + url);
 
-    testData.forEach((test) => {
-        it(`test case description`, (done) => {
-            chai.request(url)
-                .get('/')
-                .query(`color=${JSON.stringify(test.input)}`)
-                .end((err, res) => {
-                    should.not.exist(err);
-                    res.should.have.status(200);
-                    res.body.should.deep.equal(test.expected);
-                    done();
-                });
+  testData.forEach((test) => {
+    it(`test case description`, (done) => {
+      chai
+        .request(url)
+        .get("/")
+        .query(`color=${test.input}`)
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(200);
+          res.body.should.deep.equal(test.expected);
+          done();
         });
     });
+  });
 });
--- esercizi/microservices/microservice-rgbtocmyk/src/controller/HttpController.ts
+++ esercizi/microservices/microservice-rgbtocmyk/src/controller/HttpController.ts
@@ -1,13 +1,21 @@
 import {convert} from '../service/Service';
 import {Express} from 'express';
+import { CMYK, RGB } from 'color-convert/conversions';
+
+type RGBObjType = {
+    r: number,
+    g: number,
+    b: number
+}
 
 class HttpController {
     constructor(server: Express) {
         server.get('/', (req, res) => {
-            const color = JSON.parse(req.query.color) as ColorModel;
-            const convertedColor: ColorModel = convert(color);
+            const stringObj: RGBObjType = JSON.parse(req.query.color as string);
+            const color: RGB = [stringObj.r, stringObj.g, stringObj.b];
+            const convertedColor: CMYK = convert(color);
 
-            res.send(convertedColor);
+            res.send({inputRGB:color, outputCMYK:convertedColor});
         });
     }
 }
