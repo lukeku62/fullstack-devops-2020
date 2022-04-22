9e1c49dd25dfd3f542df264ae154deba46a0d198
Luca Reverberi (lukeku62) <prevebbo@gmail.com>
2022-04-22 14:50:51 +0200

    implementato ms-hsltohex

--- esercizi/microservices/microservice-hsltohex/src/controller/HttpController.componentTest.ts
+++ esercizi/microservices/microservice-hsltohex/src/controller/HttpController.componentTest.ts
@@ -2,6 +2,20 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import * as config from '../../server-config.json'
 
+type testType = {
+    input: string,
+    expected: {inputHSL: number[], outputHEX: string}
+}
+
+const testData: testType[] = [
+    {input: '{"h":0, "s":0, "l":0}', expected: {inputHSL:[0,0,0], outputHEX:'000000'}},
+    {input: '{"h":0, "s":0, "l":50}', expected: {inputHSL:[0,0,50], outputHEX:'808080'}},
+    {input: '{"h":0, "s":0, "l":100}', expected: {inputHSL:[0,0,100], outputHEX:'ffffff'}},
+    {input: '{"h":0, "s":100, "l":50}', expected: {inputHSL:[0,100,50], outputHEX:'ff0000'}},
+    {input: '{"h":0, "s":100, "l":100}', expected: {inputHSL:[0,100,100], outputHEX:'ffffff'}},
+    {input: '{"h":60, "s":100, "l":50}', expected: {inputHSL:[60,100,50], outputHEX:'ffff00'}}
+]
+
 chai.config.includeStack = true;
 const should = chai.should();
 chai.use(chaiHttp);
@@ -14,7 +28,7 @@ describe('REST API test suite description', () => {
         it(`test case description`, (done) => {
             chai.request(url)
                 .get('/')
-                .query(`color=${JSON.stringify(test.input)}`)
+                .query(`color=${test.input}`)
                 .end((err, res) => {
                     should.not.exist(err);
                     res.should.have.status(200);
--- esercizi/microservices/microservice-hsltohex/src/controller/HttpController.ts
+++ esercizi/microservices/microservice-hsltohex/src/controller/HttpController.ts
@@ -1,6 +1,6 @@
 import {convert} from '../service/Service';
 import {Express} from 'express';
-import { HEX, HSL } from 'color-convert/conversions';
+import { HSL } from 'color-convert/conversions';
 
 type HSLType = {
     h: number,
@@ -11,10 +11,11 @@ type HSLType = {
 class HttpController {
     constructor(server: Express) {
         server.get('/', (req, res) => {
-            const color: HSL = JSON.parse(req.query.color as string);
-            const convertedColor: HEX = convert(color);
+            const inputcolor: HSLType =JSON.parse(req.query.color as string);
+            const color: HSL = [inputcolor.h, inputcolor.s, inputcolor.l];
+            const convertedColor: string = convert(color).toLowerCase();
 
-            res.send(convertedColor);
+            res.send({inputHSL:color, outputHEX:convertedColor});
         });
     }
 }
