9e0b55a7ca32a6a7486bb0a52ed560c9fd3246d6
Luca Reverberi (lukeku62) <prevebbo@gmail.com>
2022-04-22 14:51:27 +0200

    cancellate cartelle commons e template

--- esercizi/microservices/microservice-template/src/service/Service.ts
+++
@@ -1,7 +0,0 @@
-import colorConverter from 'color-convert'
-
-export function convert(color: ColorModel): ColorModel {
-    const colorToConvert: ColorModel = {};
-    const convertedColor = colorConverter;
-    return {} as ColorModel;
-}
